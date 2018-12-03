f1ae4cc95a86eb370117db22afc43543d0fc1a88
anicool20 <45395114+anicool20@users.noreply.github.com>
2018-12-03 10:02:30 -0500

    Added Last name

--- GitTest.java
+++ GitTest.java
@@ -1,13 +1,11 @@
 import java.util.*;
+//Madhugiri
 public class GitTest{
 	private String test;
 
 	public GitTest(){
 		test = "This is my GitHub Test";
 	}
-	/*public String getTest(){
-		return test;
-	}*/
 	public void setTest(String test){
 		this.test = test;
 	}
